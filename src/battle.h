5bb0b7bc9ef1f56369a43db3be446ada75089aec
laureen1806 <laureen.audin@gmail.com>
2025-02-12 00:08:31 +0100

    Update battle.h

--- src/battle.h
+++ src/battle.h
@@ -1,8 +1,19 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "src/player.h"
+#include "src/supemon.h"
 
+// Prototypes des fonctions
+
+// Fonction pour utiliser un objet sur un Supémon
+void use_item(Item *item, Supemon *supemon);
+
+// Fonction pour gérer un combat entre le joueur et un Supémon sauvage
 void battle(Player *player, Supemon *wild_supemon);
 
-#endif
+#endif // BATTLE_H
